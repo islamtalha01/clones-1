6f5741e0ded74e0519f60ebe8df84dc3b399fe8a
Anees Malik <theaneesmalik@gmail.com>
2024-08-02 00:59:27 +0500

    aa

--- app/private/page.jsx
+++ app/private/page.jsx
@@ -4,7 +4,7 @@ import { createClient } from '../../utils/supabase/server'
 import { logout } from './actions'
 
 export default async function PrivatePage() {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const { data, error } = await supabase.auth.getUser()
   if (error || !data?.user) {

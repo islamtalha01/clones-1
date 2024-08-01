902eb85c479a690b94509d98a708fe2b2a687409
Anees Malik <theaneesmalik@gmail.com>
2024-08-02 01:45:01 +0500

    bug fixes

--- app/actions.js
+++ app/actions.js
@@ -13,7 +13,7 @@ export async function logout() {
   // in practice, you should validate your inputs
 
   const { error } = await supabase.auth.signOut()
-  console.log({ error })
+
   if (error) {
     redirect('/error')
   }
